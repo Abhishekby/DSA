d3a4d13dd7a399c33a54132d0e01ed0cc4a0716b
Abhishek Yadav <57463882+Abhishekby@users.noreply.github.com>
2023-01-15 16:03:54 +0530

    Update maxsubarray.cpp

--- maxsubarray.cpp
+++ maxsubarray.cpp
@@ -4,26 +4,7 @@
 using namespace std;
 
 #define int            long long int
-#define F              first
-#define S              second
-#define pb             push_back
-#define si             set <int>
-#define vi             vector <int>
-#define pii            pair <int, int>
-#define vpi            vector <pii>
-#define vpp            vector <pair<int, pii>>
-#define mii            map <int, int>
-#define mpi            map <pii, int>
-#define spi            set <pii>
-#define endl           "\n"
-#define sz(x)          ((int) x.size())
-#define all(p)         p.begin(), p.end()
-#define double         long double
-#define que_max        priority_queue <int>
-#define que_min        priority_queue <int, vi, greater<int>>
-#define print(a)       for(auto x : a) cout << x << " "; cout << endl
-#define print1(a)      for(auto x : a) cout << x.F << " " << x.S << endl
-#define print2(a,x,y)  for(int i = x; i < y; i++) cout<< a[i]<< " "; cout << endl
+
 
 inline int power(int a, int b)
 {
